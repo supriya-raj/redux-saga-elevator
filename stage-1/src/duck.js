2d559a85ce9aca7d46d0fe0f5d2b2067aee8ca0b
Supriya S <supriyas@practo.com>
2018-07-04 20:43:07 +0530

    create sub directories for levels

---
+++ stage-1/src/duck.js
@@ -0,0 +1,45 @@
+import { delay, takeLatest } from 'redux-saga'
+import { call, put } from 'redux-saga/effects'
+
+const initialState = {
+  milliseconds: 0
+}
+
+export default function reducer (currentState = initialState, action) {
+  switch (action.type) {
+    // case 'reset-clock':
+    //   return {
+    //     ...currentState,
+    //     milliseconds: 0
+    //   }
+    // case 'increment-milliseconds':
+    //   return {
+    //     ...currentState,
+    //     milliseconds: currentState.milliseconds + MINIMUM_MS
+    //   }
+    // case 'decrement-milliseconds':
+    //   if (!currentState.milliseconds) { return currentState }
+
+    //   return {
+    //     ...currentState,
+    //     milliseconds: currentState.milliseconds - MINIMUM_MS
+    //   }
+    default:
+      return currentState
+  }
+}
+
+// // actions
+// export const resetClock = () => ({ type: 'reset-clock' })
+// export const incrementMilliseconds = () => ({ type: 'increment-milliseconds' })
+// export const decrementMilliseconds = () => ({ type: 'decrement-milliseconds' })
+
+// // saga actions
+// export const startClock = () => ({ type: 'start-clock' })
+// export const pauseClock = () => ({ type: 'pause-clock' })
+// export const rewindClock = () => ({ type: 'rewind-clock' })
+
+// saga
+export function* rootSaga () {
+  //yield takeLatest(['start-clock', 'pause-clock', 'rewind-clock'], handleClockAction)
+}
